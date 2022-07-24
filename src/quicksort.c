84199435a40e94f7e9e5d1c0f5dad3e1565d9e96
dehre <loris4526@gmail.com>
2022-07-24 16:12:46 +0200

    update comments & remove invalid include from main

--- src/quicksort.c
+++ src/quicksort.c
@@ -3,13 +3,11 @@
 static int partition(int arr[], int low_idx, int high_idx);
 static void swap(int *a, int *b);
 
-// TODO LORIS: use size_t for indexes, and partition from right to left of the array
-// TODO LORIS: remove type casting in vec.c
 void quicksort(int arr[], int low_idx, int high_idx)
 {
     if (low_idx < high_idx)
     {
-        /* Items on the left of pivot will be smaller, items on the right will be bigger */
+        /* Items on the left of pivot will be smaller, items on the right will be greater */
         int pivot_idx = partition(arr, low_idx, high_idx);
         quicksort(arr, low_idx, pivot_idx - 1);
         quicksort(arr, pivot_idx + 1, high_idx);
@@ -33,6 +31,7 @@ static int partition(int arr[], int low_idx, int high_idx)
         }
     }
 
+    /* Move pivot in place */
     pivot_final_idx++;
     swap(&(arr[high_idx]), &(arr[pivot_final_idx]));
     return pivot_final_idx;
