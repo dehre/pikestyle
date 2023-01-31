356527919c9214c4b20877d5f798522af94ad84d
dehre <dehre.git@gmail.com>
2023-01-31 15:24:35 +0100

    sample code

--- apps/main.c
+++ apps/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <vec.h>
 
 int main(void)
 {
-    printf("Hello world\n");
+    struct Vec *v = vec_new(5);
+    vec_push(v, 2);
+    vec_push(v, 4);
+    for (size_t i = 0; i < vec_len(v); i++)
+    {
+        printf("v[%zu] = %d\n", i, vec_nth(v, i));
+    }
+    printf("Done\n");
     return 0;
 }
